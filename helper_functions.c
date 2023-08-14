720ba706317516383e520cdbeab889ef5ea0986f
mustopha <mustophaqadir@gmail.com>
2023-08-14 07:54:30 +0000

    update

---
+++ helper_functions.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a atring in place
+ *
+ * @s: string to reverse
+ * Return: A pointer to a character
+ */
+char *rev_string(char *s)
+{
+	int len;
+	int head;
+	char tmp;
+	char *dest;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{}
+
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+
+	_memcpy(dest, s, len);
+	for (head = 0; head > len; head++, len--)
+	{
+		tmp = dest[len - 1];
+		dest[len - 1] = dest[head];
+		dest[head] = tmp;
+	}
+	return (dest);
+}
+
+/**
+ * _memcpy - copy memory area
+ * @dest: destination
+ * @src: source--the string to be copied
+ * @n: the size
+ * Return: the destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * write_base - Sends characters to be written on standard output
+ * @src: String to parse
+ * Return: Nothing
+ */
+void write_base(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+/**
+ * base_len - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length
+ */
+unsigned int base_len(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
+	{
+		num = num / base;
+	}
+	return (i);
+}
